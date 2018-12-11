16b1fae3d9eaeebf50e7ef88c31000be4b9a1dc1
“momo@mosshi.be” <momo@mosshi.be>
2018-12-11 07:14:19 -0600

    pantalla bienvenida

--- js/app.js
+++ js/app.js
@@ -1,24 +1,118 @@
-var myGame = new SimonGame();
+var canvas = document.getElementById('circle-random'),
+    c = canvas.getContext('2d'),
+    w = window.innerWidth,
+    h = window.innerHeight,
+    opt = {
+        amount: 20,
+        direction: [
+            [1,1],
+            [-1,1],
+            [1,-1],
+            [-1,-1],
+            [1,0],
+            [-1, 0],
+            [0, 1],
+            [0, -1]
+        ],
+        speed: .5,
+        size: 15
+    },
+    Particle = function(x, y){
+        this.x = x || 0;
+        this.y = y || 0;
+        this.direction = opt.direction[Math.floor(Math.random() * opt.direction.length)];
+        this.speed = opt.speed * Math.random();
+        this.size = opt.size + Math.random() * 20;
+        var color = randomColor();
 
-$(document).ready(function () {
-    myGame.startGame();
+        this.update = function(){
+
+            this.x += this.direction[0] * this.speed;
+            this.y += this.direction[1] * this.speed;
+
+        };
+
+        this.render = function() {
+            this.colision();
+
+            c.fillStyle = 'rgba(' + color[0] + ','+ color[1] +','+ color[2] ;
+            c.beginPath();
+            c.arc(this.x, this.y, this.size , 0, 5 * Math.PI, false);
+            c.fill();
+
+        };
+
+        this.colision = function() {
+            if(this.x < 0 - this.size) {
+                this.x = w;
+            }
+            if(this.x > w + this.size) {
+                this.x = 0;
+            }
+            if(this.y < 0 - this.size) {
+                this.y = h;
+            }
+            if(this.y  > h + this.size) {
+                this.y = 0;
+            }
+
+        }
+
+    },
+    circle = [];
+
+
+canvas.width = w;
+canvas.height = h;
+
+window.addEventListener('resize', function() {
+    w = window.innerWidth;
+    h = window.innerHeight;
+    canvas.width = w;
+    canvas.height = h;
 });
 
+function randomColor() {
+    var color = [
+        [48, 35, 174],
+        [180, 236, 81],
+        [245, 81, 95],
+        [48, 35, 174],
+        [250, 217, 97],
+        [124, 179, 252],
+        [175, 101, 189],
+        [182, 64, 106],
+        [77, 53, 169]
+    ];
 
-$(document).ready(function () {
-    $('button').click(function () {
-        var colorInput   = $(this).attr("id");
-        var currentColor = myGame.sequence[myGame.userClickCount];
+    return color[Math.floor(Math.random() * color.length)];
+}
 
-        if (currentColor !== colorInput) {
-            // game over
-            alert('GAME OVER');
-        }
 
-        myGame.userClickCount += 1;
+(function setup() {
 
-        if (myGame.userClickCount === myGame.sequence.length) {
-            alert('Sequence complete!');
-        }
+    for(var i = 0; i < opt.amount; i++) {
+        circle.push(new Particle(
+            Math.random() * w,
+            Math.random() * h
+        ));
+    }
+
+    setTimeout(function(){
+        window.requestAnimationFrame(loop);
+    }, 1000 / 30);
+})();
+
+function loop() {
+    c.clearRect(0, 0, w, h);
+
+    circle.map(function(C) {
+        C.update();
+        C.render();
     });
-});
+
+    setTimeout(function(){
+        window.requestAnimationFrame(loop);
+    }, 1000 / 50);
+};
+
